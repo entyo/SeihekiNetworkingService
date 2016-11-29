302de00f786beea4a27dfd145dfa83ec17871ef9
e_ntyo <entyo.contact@gmail.com>
2016-11-29 14:37:24 +0900

    :sparkle: Let's Encryptを使う

--- server.go
+++ server.go
@@ -1,19 +1,35 @@
 // web.go
 package main
 
-import "github.com/gin-gonic/gin"
-import "net/http"
-import "os"
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	// Initialize server
 	router := gin.Default()
 	router.Static("/assets", "./assets")
 	router.LoadHTMLGlob("view/*")
+
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "Share adult contents you love!",
 		})
 	})
+
+	// For Let’s Encrypt
+	router.GET("/.well-known/acme-challenge/:value", func(c *gin.Context) {
+		req := os.Getenv("LETSENCRYPT_REQUEST")
+		res := os.Getenv("LETSENCRYPT_RESPONSE")
+
+		if c.Param("value") == req {
+			c.String(http.StatusOK, res)
+		}
+	})
+
 	if os.Getenv("PORT") == "" {
 		router.Run(":" + "8080")
 	} else {
